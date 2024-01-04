2d84f1b6d47ede64199b08d16d592defab72d3e0
Genaro Rossi <genaros.gr@gmail.com>
2024-01-04 12:37:16 -0300

    añado cancion con url firebase

--- projects/03-audio-reproductor/src/data/tracks.js
+++ projects/03-audio-reproductor/src/data/tracks.js
@@ -14,5 +14,10 @@ export const tracks = [
       title: "Madza - Persistence",
       tags: ["dubstep"],
     },
+    {
+        url: "https://firebasestorage.googleapis.com/v0/b/react-firebase-1e173.appspot.com/o/audio%2Fa726da2d-8ba0-4615-a361-267d1d0953e6?alt=media&token=821837e9-ec90-44d6-880f-70e3db6f5cd5",
+        title: "Asi es la vida - Maria Becerra",
+        tags: ["bachata"],
+      },
   ];
   export default tracks
