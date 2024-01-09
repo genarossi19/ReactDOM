deac79d8aa2a2e69c9a85428f2b76f5f0de25d34
Genaro Rossi <genaros.gr@gmail.com>
2024-01-09 11:48:53 -0300

    probando tailwind

---
+++ projects/07-todo-list/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Title from "./components/Title.jsx";
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-slate-900 min-h-screen h-full text-gray-100 flex items-center justify-center py-20 px-5">
+      <div className="container flex flex-col max-w-xl">
+      <Title/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ projects/07-todo-list/src/components/Title.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Title() {
+  return (
+    <div className="text-5xl font-bold tracking-wide"><h1>Todo List</h1></div>
+  )
+}
+
+export default Title
