1a1b03756a792653c23eb102d22dd3c7d5ce0738
Genaro Rossi <genaros.gr@gmail.com>
2024-01-22 13:05:31 -0300

    renderizar varias cards con llamada a la api

--- projects/11-react-query/src/api/queries/getPokemons.js
+++ projects/11-react-query/src/api/queries/getPokemons.js
@@ -1,5 +1,22 @@
-export const getPokemons = async ({url=null, offset=0, limit=20})=>{
-
-   
-
-}
+export const getPokemons = async ({ url = null, offset = 0, limit = 20 }) => {
+    const urlToFetch = url ?? `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+  
+    try {
+      const response = await fetch(urlToFetch);
+  
+      if (!response.ok) {
+        throw new Error(`Failed to fetch: ${response.status} - ${response.statusText}`);
+      }
+  
+      return response.json();
+    } catch (error) {
+      console.error('Error in getPokemons:', error);
+  
+      if (error instanceof TypeError && error.message === 'Failed to fetch') {
+        console.error('Network error. Please check your internet connection.');
+      }
+  
+      throw new Error(`Error fetching data: ${error.message}`);
+    }
+  };
+  
--- projects/11-react-query/src/components/PokemonList.jsx
+++ projects/11-react-query/src/components/PokemonList.jsx
@@ -1,19 +1,29 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 import PokemonCard from "./PokemonCard";
+import { getPokemons } from "../api/queries/getPokemons";
 
 function PokemonList() {
+  const { data } = useInfiniteQuery({
+    getNextPageParam: (lastPage) => lastPage.next,
+    queryFn: ({ pageParam }) => getPokemons({ url: pageParam }),
+    queryKey: ["pokemons"],
+  });
 
- 
+  console.log(data);
+  console.log(data?.pages);
+  console.log(data?.pages?.[0]?.results);
+
   return (
     <>
       <h1 className="md:text-8xl text-5xl font-bol mb-6 ">Pokemons</h1>
-      
-   
       <section className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-flow-row gap-3 w-full ">
-        
-       <PokemonCard/>
-       
-
+        {data &&
+          data.pages.map((page) => {
+            return page.results.map((pokemon) => (
+              <PokemonCard key={pokemon.name} pokemonName={pokemon.name} />
+            ));
+          })}
       </section>
     </>
   );
