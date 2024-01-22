ae854be9fe0a34228befa02e5d7cce0ffe045fee
Genaro Rossi <genaros.gr@gmail.com>
2024-01-22 13:38:15 -0300

    uso de infinte Scrollc

--- projects/11-react-query/README.md
+++ projects/11-react-query/README.md
@@ -1,1 +1,3 @@
 npm i @tanstack/react-query-devtools
+
+https://www.npmjs.com/package/react-intersection-observer
--- projects/11-react-query/src/components/PokemonList.jsx
+++ projects/11-react-query/src/components/PokemonList.jsx
@@ -1,14 +1,33 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-
+import {useInView} from 'react-intersection-observer'
 import PokemonCard from "./PokemonCard";
 import { getPokemons } from "../api/queries/getPokemons";
+import { Fragment, useEffect } from "react";
 
 function PokemonList() {
-  const { data } = useInfiniteQuery({
+  const { data, status, fetchNextPage, error } = useInfiniteQuery({
     getNextPageParam: (lastPage) => lastPage.next,
     queryFn: ({ pageParam }) => getPokemons({ url: pageParam }),
     queryKey: ["pokemons"],
   });
+
+  const {ref, inView} = useInView({
+    threshold: 0,
+  })
+
+  useEffect(()=>{
+    if(inView){
+      fetchNextPage()
+    }
+  }, [fetchNextPage, inView])
+
+  if(status==='pending'){
+    return <p>Loading...</p>
+  }
+
+  if(status === 'error'){
+    return <p>Ha ocurrido algun error: {error.message}</p>
+  }
 
   console.log(data);
   console.log(data?.pages);
@@ -20,8 +39,13 @@ function PokemonList() {
       <section className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-flow-row gap-3 w-full ">
         {data &&
           data.pages.map((page) => {
-            return page.results.map((pokemon) => (
-              <PokemonCard key={pokemon.name} pokemonName={pokemon.name} />
+            return page.results.map((pokemon, index) => (
+              <Fragment key={pokemon.name}>
+                 {index === page.results.length - 1 ? (
+                  <div ref={ref}> <PokemonCard pokemonName={pokemon.name}/></div>
+                 ): (<PokemonCard pokemonName={pokemon.name}/>)}
+              </Fragment>
+             
             ));
           })}
       </section>
