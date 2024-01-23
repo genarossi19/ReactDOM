37a4961530457cdbdefb279a09709bf0a0991b26
Genaro Rossi <genaros.gr@gmail.com>
2024-01-23 11:25:33 -0300

    fragment

--- projects/11-react-query/src/components/LottieLoading.jsx
+++ projects/11-react-query/src/components/LottieLoading.jsx
@@ -1,16 +1,16 @@
 import Lottie from "lottie-react";
 import animation from "../assets/animations/pokeball.json";
 
-import './LottieLoading.css'
+import "./LottieLoading.css";
+import { Fragment } from "react";
 
 const LottieLoading = () => {
   return (
-    <div className="loading-animation">
-  <Lottie className="animation" animationData={animation}  />
-</div>
-
-
-  )
-
-}
+    <Fragment>
+      <div className="loading-animation">
+        <Lottie className="animation" animationData={animation} />
+      </div>
+    </Fragment>
+  );
+};
 export default LottieLoading;
