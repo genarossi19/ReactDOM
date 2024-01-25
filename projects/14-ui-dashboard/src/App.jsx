50c9a6e5e81f02e969395ad709230ee65c199645
Genaro Rossi <genaros.gr@gmail.com>
2024-01-25 13:48:55 -0300

    creacion del proyecto

---
+++ projects/14-ui-dashboard/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css"; //lo dejo por si quiero implementar css puro
+
+function App() {
+  return (
+    <div id="container" className="min-h-screen">
+      <div id="bloque1" className="bg-blue-400">Blue</div>
+      <div id="bloque2" className="bg-green-300">Green</div>
+    </div>
+  );
+}
+
+export default App;
