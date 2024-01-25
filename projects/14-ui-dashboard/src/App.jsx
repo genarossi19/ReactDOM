684fd05ece8230c3415ebb338765e49ae18750c8
Genaro Rossi <genaros.gr@gmail.com>
2024-01-25 13:57:38 -0300

    division de la pantalla

--- projects/14-ui-dashboard/src/App.jsx
+++ projects/14-ui-dashboard/src/App.jsx
@@ -3,9 +3,10 @@ import "./App.css"; //lo dejo por si quiero implementar css puro
 
 function App() {
   return (
-    <div id="container" className="min-h-screen">
-      <div id="bloque1" className="bg-blue-400">Blue</div>
-      <div id="bloque2" className="bg-green-300">Green</div>
+    <div id="container" className="min-h-screen grid grid-cols-6 ">
+      {/* span-1 + span-2 = grid-cols-6 */}
+      <div id="bloque1" className="bg-blue-400 col-span-1">Blue</div>
+      <div id="bloque2" className="bg-green-300 col-span-5">Green</div>
     </div>
   );
 }
