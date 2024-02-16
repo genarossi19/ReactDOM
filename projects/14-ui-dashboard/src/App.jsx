b859b4b7d7bb3bc027b88ef518db33fed074a38d
Genaro Rossi <genaros.gr@gmail.com>
2024-02-16 11:46:53 -0300

    vector&logout

--- projects/14-ui-dashboard/src/App.jsx
+++ projects/14-ui-dashboard/src/App.jsx
@@ -8,35 +8,36 @@ import vector from "../public/vector.svg";
 
 function App() {
   return (
-    <div id="container" className="min-h-screen grid grid-cols-6 ">
+    <div id="container" className="min-h-screen grid grid-cols-6">
       {/* span-1 + span-2 = grid-cols-6 */}
-      <div id="sidebar" className=" col-span-1 p-8 border border-e-gray">
+      <div id="sidebar" className=" col-span-1 p-4 border border-e-gray ">
         {/* Logo */}
         <div className="text-center py-8">
           <h1 className="uppercase font-bold tracking-[4px]">Tu Logo</h1>
         </div>
-
-        {/* Menú con diferentes iconos y etiquetas */}
-        <nav>
-          <ul>
-            <li>
-              <MenuItem icon={MdSpaceDashboard} label="Dashboard" link="#" />
-            </li>
-            <li>
-              <MenuItem icon={MdOutlineWork} label="Job Board" link="#" />
-            </li>
-            <li>
-              <MenuItem icon={FaCalendar} label="Calendar" link="#" />
-            </li>
-            <li>
-              <MenuItem icon={FaMessage} label="Message" link="#" />
-            </li>
-          </ul>
-        </nav>
-        Image & logout
-        <div className="flex flex-col gap-4 ">
-          <img src={vector} alt="vector" className="" />
-          <MenuItem icon={MdLogout} label="Logout" link="#" />
+        <div className="bg-red-200 flex flex-col justify-between min-h-[500px] max-h-screen">
+          {/* Menú con diferentes iconos y etiquetas */}
+          <nav>
+            <ul>
+              <li>
+                <MenuItem icon={MdSpaceDashboard} label="Dashboard" link="#" />
+              </li>
+              <li>
+                <MenuItem icon={MdOutlineWork} label="Job Board" link="#" />
+              </li>
+              <li>
+                <MenuItem icon={FaCalendar} label="Calendar" link="#" />
+              </li>
+              <li>
+                <MenuItem icon={FaMessage} label="Message" link="#" />
+              </li>
+            </ul>
+          </nav>
+          {/* Image & logout */}
+          <div className="flex flex-col gap-4 ">
+            <img src={vector} alt="vector" className="" />
+            <MenuItem icon={MdLogout} label="Logout" link="#" />
+          </div>
         </div>
       </div>
       <div className="col-span-5 p-4 ">
@@ -49,3 +50,4 @@ function App() {
 }
 
 export default App;
+
