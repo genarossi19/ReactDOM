9c268fd8b0ce8082e02d488b7602697beb691151
Genaro Rossi <genaros.gr@gmail.com>
2024-03-01 14:02:30 -0300

    form simple

--- projects/18-react-expense-tracker/src/components/TransactionForm.jsx
+++ projects/18-react-expense-tracker/src/components/TransactionForm.jsx
@@ -1,9 +1,13 @@
-
-
 function TransactionForm() {
   return (
-    <div>TransactionForm</div>
-  )
+    <div>
+      <form>
+        <input type="text" placeholder="Ingresa una descripción" />
+        <input type="number" step="0.01" placeholder="00.00"className="text-gray-800" />
+        <button>Add transaction</button>
+      </form>
+    </div>
+  );
 }
 
-export default TransactionForm
+export default TransactionForm;
